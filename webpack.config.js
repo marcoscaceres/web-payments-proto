d26b998b6b16f8b98adf1c6006d76651ef9e6ea1
Marcos Caceres <marcos@marcosc.com>
2017-03-03 17:28:50 +1100

    chore(webpack.config): relative path for build

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     path: __dirname,
-    filename: "build/bundle.js"
+    filename: "./build/bundle.js"
   },
   resolve: {
     extensions: [".js", ".json"]
