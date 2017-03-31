ca7c294bce904b9b59eaae1717dbb4b660376eb0
Marcos Caceres <marcos@marcosc.com>
2017-03-31 18:21:19 +1100

    feat(AutofillDB): match Gecko schema

--- src/AutofillDB.js
+++ src/AutofillDB.js
@@ -1,9 +1,12 @@
 import Dexie from "dexie";
 
 const db = new Dexie("Autofill");
-db.version(1).stores({
-  addresses: `organization, name,street, suburb,state, postalcode, country phone,email`,
-  creditCards: `name-on-card, card-number, expiry-month, expire-year`,
-});
+
+db
+  .version(1)
+  .stores({
+    addresses: "&guid, organization, street-address, address-level2, address-level1, postal-code, country, tel, email, timeCreated, timeLastUsed, timeLastModified, timesUsed",
+    cards: "&card-number, name-on-card, expiry-month, expire-year, valid-from-month, valid-from-year",
+  });
 
 export default db;
